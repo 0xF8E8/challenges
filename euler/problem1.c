ff3cbbc8753589036e01b527afb30fc7499a3a9b
0xF8E8 <anthony.j.lynn@openmailbox.org>
2014-12-13 11:44:33 -0500

    Fixed output statement and ensured C89 compatibility in the for loop

--- euler/problem1.c
+++ euler/problem1.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 //Store sum of multiples of 3 and 5 below 1000
 int sum = 0;
-
+int i = 0;
 int main () {
-    //Loop through natural numbers below 1000, adding if divisible by 3 or 5 
+    //Loop through natural numbers below 1000, adding if divisible by 3 or 5
     for (i = 0; i < 1000; i++) {
         //The 'not' symbol makes a quick test for equality, simply flipping
         //the result of the modulo operation, 0, to a truth value, 1.
@@ -12,8 +11,6 @@ int main () {
             sum += i;
         }
     }
-    
-    puts (sum);
-    
+    printf  ("%d\n", sum);
     return 0;
 }
